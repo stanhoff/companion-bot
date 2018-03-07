6bfa240a90bf8315e5a90c2202ba35d82b63588a
stanhoff <37093067+stanhoff@users.noreply.github.com>
2018-03-07 00:42:24 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,10 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+    client.on('message', message => {
+    if (message.content === 'ping!') {
+    	message.reply('pong!');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
